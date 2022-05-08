0f3660317c9edb9441e8f729703e2f867c1abc1d
Leinier Caraballo Yanes <94509566+leiniercy@users.noreply.github.com>
2022-05-08 19:03:42 -0400

    modifique la pagina del catalogo
    cree los post con la informacion de los libros
    cree el input de filtro
    falta implementar los ordenamientos por orden alfabetico y asignatura

--- src/main/java/trabajodediploma/views/catalogo/CatalogoView.java
+++ src/main/java/trabajodediploma/views/catalogo/CatalogoView.java
@@ -1,67 +1,140 @@
 package trabajodediploma.views.catalogo;
 
-import com.vaadin.flow.component.HasComponents;
-import com.vaadin.flow.component.HasStyle;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Main;
-import com.vaadin.flow.component.html.OrderedList;
-import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import trabajodediploma.data.entity.Libro;
+import trabajodediploma.data.service.LibroService;
 import trabajodediploma.views.MainLayout;
 
 @PageTitle("Catalogo")
 @Route(value = "catalogo-libros", layout = MainLayout.class)
 @AnonymousAllowed
-public class CatalogoView extends Main implements HasComponents, HasStyle {
+public class CatalogoView extends Div {
 
-    private OrderedList imageContainer;
+    List<Libro> libros;
+    LibroService libroService;
+    Select<String> sortBy;
+    TextField filtrar;
+    Div content;
+    private Div header;
 
-    public CatalogoView() {
-        constructUI();
+    public CatalogoView(@Autowired LibroService libroService) {
+        addClassNames("catalogo-view");
+        this.libroService = libroService;
+        this.libros = libroService.findAll();
+        Configuracion();
+        barraDeNavegacion();
+        post(libros);
+        add(header, content);
+    }
 
-        imageContainer.add(new CatalogoViewCard("Snow mountains under stars",
-                "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"));
-        imageContainer.add(new CatalogoViewCard("Snow covered mountain",
-                "https://images.unsplash.com/photo-1512273222628-4daea6e55abb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"));
-        imageContainer.add(new CatalogoViewCard("River between mountains",
-                "https://images.unsplash.com/photo-1536048810607-3dc7f86981cb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80"));
-        imageContainer.add(new CatalogoViewCard("Milky way on mountains",
-                "https://images.unsplash.com/photo-1515705576963-95cad62945b6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80"));
-        imageContainer.add(new CatalogoViewCard("Mountain with fog",
-                "https://images.unsplash.com/photo-1513147122760-ad1d5bf68cdb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"));
-        imageContainer.add(new CatalogoViewCard("Mountain at night",
-                "https://images.unsplash.com/photo-1562832135-14a35d25edef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=815&q=80"));
+    private void Configuracion() {
+        header = new Div();
+        header.addClassName("header");
+        content = new Div();
+        content.addClassName("content");
+        
+        
+    }
+    
+    private void OrdenamientoPorOrdenAlfabetico(){
+        
+    }
+    private void OrdenamientoPorAsignatura(){
+        
+    }
+
+    private void barraDeNavegacion() {
+        sortBy = new Select<>();
+        sortBy.addClassName("select");
+        sortBy.setLabel("Ordenar por");
+        sortBy.setItems("Orden Alfabético", "Asignatura");
+        sortBy.setValue("Orden Alfabético");
+
+        filtrar = new TextField();
+        filtrar.addClassName("filtrar");
+        filtrar.setLabel("Filtrar por");
+        filtrar.setPlaceholder("");
+        filtrar.setPrefixComponent(VaadinIcon.SEARCH.create());
+        filtrar.setClearButtonVisible(true);
+        filtrar.setValueChangeMode(ValueChangeMode.EAGER);
+        filtrar.addValueChangeListener(event -> {
+            if (filtrar.getValue().isEmpty()) {
+                libros = libroService.findAll();
+                content.removeAll();
+                post(libros);
+            } else {
+                libros = libros.parallelStream()
+                        .filter(lib -> StringUtils.containsIgnoreCase(lib.getAutor(),filtrar.getValue())
+                        || StringUtils.containsIgnoreCase(lib.getTitulo(),filtrar.getValue())
+                        || StringUtils.containsIgnoreCase(Integer.toString(lib.getVolumen()), filtrar.getValue())
+                        || StringUtils.containsIgnoreCase(Integer.toString(lib.getTomo()), filtrar.getValue())
+                        || StringUtils.containsIgnoreCase(Integer.toString(lib.getParte()), filtrar.getValue())
+                        )
+                        .collect(Collectors.toList());
+                content.removeAll();
+                post(libros);
+            }
+        });
+        header.add(filtrar, sortBy);
+    }
+
+    private void post(List<Libro> libros) {
+
+        for (int i = 0; i < libros.size(); i++) {
+
+            Image imagen = new Image(libros.get(i).getImagen(), libros.get(i).getTitulo());
+            imagen.addClassName("imagen");
+            Div imagenDiv = new Div(imagen);
+            imagenDiv.addClassName("imagen-div");
+            Div imagenContainerDiv = new Div(imagenDiv);
+            imagenContainerDiv.addClassName("imagen-div-div");
+
+            Div info = new Div();
+            info.addClassName("info");
+
+            H1 titulo = new H1("Título: " + libros.get(i).getTitulo());
+            H2 autor = new H2("Autor: " + libros.get(i).getAutor());
+            H3 volumen = new H3("Vólumen: " + libros.get(i).getVolumen().toString());
+            H3 tomo = new H3("Tomo: " + libros.get(i).getTomo().toString());
+            H3 parte = new H3("Parte: " + libros.get(i).getParte().toString());
+
+            info.add(titulo, autor);
+            if (libros.get(i).getVolumen() != null) {
+                info.add(volumen);
+            } else if (libros.get(i).getParte() != null) {
+                info.add(parte);
+            } else if (libros.get(i).getTomo() != null) {
+                info.add(tomo);
+            }
+
+            Button leerButton = new Button("LEER");
+            leerButton.addClassName("leer-button");
+            Div leerDiv = new Div(leerButton);
+            leerDiv.addClassName("leer-div");
+
+            Div post = new Div(imagenContainerDiv, info, leerDiv);
+            post.addClassName("post");
+
+            content.add(post);
+        }
 
     }
 
-    private void constructUI() {
-        addClassNames("catalogo-view", "max-w-screen-lg", "mx-auto", "pb-l", "px-l");
-
-        HorizontalLayout container = new HorizontalLayout();
-        container.addClassNames("items-center", "justify-between");
-
-        VerticalLayout headerContainer = new VerticalLayout();
-        H2 header = new H2("Beautiful photos");
-        header.addClassNames("mb-0", "mt-xl", "text-3xl");
-        Paragraph description = new Paragraph("Royalty free photos and pictures, courtesy of Unsplash");
-        description.addClassNames("mb-xl", "mt-0", "text-secondary");
-        headerContainer.add(header, description);
-
-        Select<String> sortBy = new Select<>();
-        sortBy.setLabel("Sort by");
-        sortBy.setItems("Popularity", "Newest first", "Oldest first");
-        sortBy.setValue("Popularity");
-
-        imageContainer = new OrderedList();
-        imageContainer.addClassNames("gap-m", "grid", "list-none", "m-0", "p-0");
-
-        container.add(header, sortBy);
-        add(container, imageContainer);
-
-    }
 }
