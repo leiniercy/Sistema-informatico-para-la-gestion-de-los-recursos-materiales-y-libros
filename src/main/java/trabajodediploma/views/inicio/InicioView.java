e13b463f0a3f9fdbe48e2ef7411cb2a06f3bf126
Leinier Caraballo Yanes <leiniercaraballo08@gmail.com>
2022-08-09 16:46:26 -0500

    modifique la pagina de inicio
    aun queda cambiar el logo de la pagina
    pienso cambiar la descripcion de la pagina
    falta hacer el diseno grid css

--- src/main/java/trabajodediploma/views/inicio/InicioView.java
+++ src/main/java/trabajodediploma/views/inicio/InicioView.java
@@ -1,10 +1,17 @@
 package trabajodediploma.views.inicio;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.dependency.JavaScript;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.AnchorTarget;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Footer;
 import com.vaadin.flow.component.html.H1;
@@ -38,7 +45,11 @@ import trabajodediploma.views.MainLayout;
 import trabajodediploma.views.footer.MyFooter;
 import trabajodediploma.views.login.crear_informacion_perfil.CrearInformacionPerfilView;
 
-@PageTitle("Inicio")
+@Tag("div")
+// @CssImport("./styles/css/bootstrap.css")
+//@JavaScript("flow-frontend://src/js/bootstrap")
+
+@PageTitle("Genius")
 @Route(value = "inicio", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
 @AnonymousAllowed
@@ -46,7 +57,7 @@ public class InicioView extends Div {
 
     private CrearInformacionPerfilView crearPerfil;
     private MyFooter footer;
-    private Div content;
+    private Div container;
     private List<Estudiante> estudiantes;
     private List<Trabajador> trabajadores;
     private Dialog dialog;
@@ -57,6 +68,12 @@ public class InicioView extends Div {
     private GrupoService grupoService;
     private Div header;
     private User user;
+    private Div container_seccion1;
+    private Div container_seccion2;
+    private Div container_seccion3;
+    private Div container_seccion4;
+
+
 
     public InicioView(
             @Autowired AuthenticatedUser authenticatedUser,
@@ -75,121 +92,166 @@ public class InicioView extends Div {
         trabajadores = trabajadorService.findAll();
         Configuracion();
 
-        Optional<User> maybeUser = authenticatedUser.get();
-        if (maybeUser.isPresent()) {
-            user = maybeUser.get();
-            /*crear perfil*/
-            dialog = new Dialog();
-            crearPerfil = new CrearInformacionPerfilView(user, estudianteService, trabajadorService, areaService, grupoService, dialog);
-            /*Fin -> crear perfil
-            * Header crear  perfil usuario*/
-            Button closeButton = new Button(new Icon("lumo", "cross"), (e) -> dialog.close());
-            closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-            Span title = new Span("Perfil");
-            Div titleDiv = new Div(title);
-            titleDiv.addClassName("div-perfil-title");
-            Div buttonDiv = new Div(closeButton);
-            buttonDiv.addClassName("div-perfil-button");
-            header = new Div(titleDiv, buttonDiv);
-            header.addClassName("div-perfil-header");
-            dialog.add(header, crearPerfil);
-            /*Fin -> Header crear perfil usuario*/
-            estudiantes = estudiantes.parallelStream().filter(event -> event.getUser().getUsername().equals(user.getUsername())).collect(Collectors.toList());
-            trabajadores = trabajadores.parallelStream().filter(event -> event.getUser().getUsername().equals(user.getUsername())).collect(Collectors.toList());
-
-            if (estudiantes.size() == 0 && trabajadores.size() == 0) {
-                dialog.open();
-                add(content, Links(), footer);
-            } else {
-                add(content, Links(), footer);
-            }
-
-        } else {
-            add(content, Links(), footer);
-        }
+        add(container/*,footer*/);
     }
 
     private void Configuracion() {
-        content = new Div();
-        content.addClassName("div-content");
+        container = new Div();
+        container.addClassName("div-container");
+        container_seccion1 = new Div( Seccion1() );
+        container_seccion1.addClassName("div-container-seccion1");
+        container_seccion2 = new Div(Seccion2());
+        container_seccion2.addClassName("div-container-seccion2");
+        container_seccion3 = new Div(Seccion3());
+        container_seccion3.addClassName("div-container-seccion3");
+        container_seccion4 = new Div(Seccion4());
+        container_seccion4.addClassName("div-container-seccion4");
+
+        container.add(container_seccion1, container_seccion2, container_seccion3, container_seccion4);
         footer = new MyFooter();
         footer.addClassName("footer");
-        divDescripcionSistema();
+        
+    }
+  
+    private Component Seccion1 (){
+        
+        Div seccion1 = new Div();
+        seccion1.addClassName("seccion1");
+
+        /*Logo libro*/
+        Div seccion1_image = new Div();
+        seccion1_image.addClassNames("seccion1-image");
+        Div img_container = new Div();
+        img_container.addClassNames("seccion1-image-container");
+        Image image1 = new Image("images/logo_pag_2.png", "Logo");
+        image1.addClassNames("seccion1-image-container-img");
+        img_container.add(image1);
+        seccion1_image.add(img_container);
+        /*Logo libro*/
+        /*Enlace*/
+        Div seccion1_info = new Div();
+        seccion1_info.addClassName("seccion1-info");
+        Anchor enlace = new Anchor("catalogo-libros","Ver Cátalogo");
+        enlace.addClassName("seccion1-info-enlace");
+        Paragraph parrafoInfo = new Paragraph("Sistema de Gestión Académica destinado al control y distribución de recursos materiales correspondientes a la Facultad 4 .....");
+        parrafoInfo.addClassName("seccion1-info-parrafo");
+        seccion1_info.add(enlace,parrafoInfo);
+        /*Enlace*/
+        seccion1.add(seccion1_image, seccion1_info);
+        return seccion1;
+    }
+   
+    private Component Seccion2 (){      
+        Div seccion2 = new Div();
+        seccion2.addClassNames("seccion2");
+        return seccion2;
     }
 
-    private void divDescripcionSistema() {
-        /*titulo*/
-        H1 title = new H1("SISTEMA DE CONTROL Y DISTRIBUCIÓN DE RECURSOS MATERIALES");
-        title.addClassName("h1-title");
-        Div divTitle = new Div(title);
-        divTitle.addClassName("div-h1-title");
-        /*titulo*/
+    private Component Seccion3(){
+        Div seccion3 = new Div();
+        seccion3.addClassName("seccion3");
 
- /*imagen*/
-        Image img = new Image("images/presentacion.png", "Logo");
-        img.addClassName("image");
-        Div imageDiv = new Div(img);
-        imageDiv.addClassName("div-image");
-        /*imagen*/
- /*descripcion*/
-        Paragraph info = new Paragraph("Aplicación destinada al cuidadado, protección y "
-                + "conservación de los recursos materiales que posee la facutad 4, sobre la base"
-                + "de un uso adecuado en función del control de la entrega del módulos a estudiantes "
-                + "y trabajadores además de la entrada y salida de libros del almacén.");
-        info.addClassName("info");
-        Div divInfo = new Div(imageDiv, info);
-        divInfo.addClassName("div-info");
-        /*descripcion*/
+    
+        Div seccion3ImagenContainer = new Div();
+        seccion3ImagenContainer.addClassName("seccion3-imagen-container");
+        Div seccion3Imagen = new Div();
+        seccion3Imagen.addClassName("seccion3-imagen");
+        Image imagen = new Image("images/uci.png", "Universidad de las Ciencias Informáticas");
+        seccion3Imagen.add(imagen);
+        seccion3ImagenContainer.add(seccion3Imagen);
+    
 
-        content.add(divTitle, divInfo);
+        Div seccion3Info = new Div();
+        seccion3Info.addClassName("seccion3-info");
+        H2 seccion3Title = new H2("Acerca de nosotros");
+        Paragraph seccion3Informacion = new Paragraph("Centro de estudios universitarios radicado en La Habana, Cuba, en el municipio de La Lisa. Nacido como un proyecto de la Revolución Cubana, denominado al principio 'Proyecto Futuro', con dos objetivos: informatizar el país y desarrollar la industria del Software para contribuir al desarrollo económico del mismo.");
+        Anchor seccion3Enlace = new Anchor("https://www.uci.cu/","Leer más...");
+        seccion3Info.add(seccion3Title, seccion3Informacion, seccion3Enlace);
+
+        seccion3.add(seccion3ImagenContainer, seccion3Info);
+        return seccion3;
     }
 
-    private Component Links() {
+    private Component Seccion4() {
 
-        HorizontalLayout layout = new HorizontalLayout();
+        Div seccion4 = new Div();        
+        seccion4.addClassName("seccion4");
 
+
+        /*SITIOS DE INTERÉS*/ 
         H2 sitiosInteres = new H2("SITIOS DE INTERÉS");
-        HorizontalLayout link1 = new HorizontalLayout(VaadinIcon.LINK.create(), new H3("Repositorio Institucional"));
-        link1.setAlignItems(FlexComponent.Alignment.BASELINE);
-        Anchor repositorioInstitucional = new Anchor("https://repositorio.uci.cu/jspui/", link1);
-        HorizontalLayout link2 = new HorizontalLayout(VaadinIcon.LINK.create(), new H3("Biblioteca"));
-        link2.setAlignItems(FlexComponent.Alignment.BASELINE);
-        Anchor biblioteca = new Anchor("https://biblioteca.uci.cu/", link2);
-        HorizontalLayout link3 = new HorizontalLayout(VaadinIcon.LINK.create(), new H3("Internos"));
-        link3.setAlignItems(FlexComponent.Alignment.BASELINE);
-        Anchor internos = new Anchor("https://internos.uci.cu/", link3);
+        
+        Anchor repositorioInstitucional = new Anchor("https://repositorio.uci.cu/jspui/");
+        repositorioInstitucional.addClassName("seccion4-link");
+        repositorioInstitucional.add(VaadinIcon.LINK.create());
+        repositorioInstitucional.add("Repositorio Institucional");
+        repositorioInstitucional.setTarget("_BLANK");
+
+        Anchor biblioteca = new Anchor("https://biblioteca.uci.cu/");
+        biblioteca.addClassName("seccion4-link");
+        biblioteca.add(VaadinIcon.LINK.create());
+        biblioteca.add("Biblioteca");
+        biblioteca.setTarget("_BLANK");
+
+        Anchor internos = new Anchor("https://internos.uci.cu/");
+        internos.addClassName("seccion4-link");
+        internos.add(VaadinIcon.LINK.create());
+        internos.add("Internos");
+        internos.setTarget("_BLANK");
+
         VerticalLayout sitiosDeInteres = new VerticalLayout(sitiosInteres, repositorioInstitucional, biblioteca, internos);
+        /*SITIOS DE INTERÉS*/
 
+        /*OTROS SITIOS*/
         H2 otroSitios = new H2("OTROS SITIOS");
-        HorizontalLayout link4 = new HorizontalLayout(VaadinIcon.LINK.create(), new H3("Portal UCI"));
-        link4.setAlignItems(FlexComponent.Alignment.BASELINE);
-        Anchor portalUCI = new Anchor("https://www.uci.cu/", link4);
-        HorizontalLayout link5 = new HorizontalLayout(VaadinIcon.LINK.create(), new H3("Intranet"));
-        link5.setAlignItems(FlexComponent.Alignment.BASELINE);
-        Anchor intranet = new Anchor("https://intranet.uci.cu/", link5);
-        HorizontalLayout link6 = new HorizontalLayout(VaadinIcon.LINK.create(), new H3("Periódico Mella"));
-        link6.setAlignItems(FlexComponent.Alignment.BASELINE);
-        Anchor periodicoMella = new Anchor("https://periodico.uci.cu/", link6);
+
+        Anchor portalUCI = new Anchor("https://www.uci.cu/");
+        portalUCI.addClassName("seccion4-link");
+        portalUCI.add(VaadinIcon.LINK.create());
+        portalUCI.add("Portal UCI");
+        portalUCI.setTarget("_BLANK");
+
+        Anchor intranet = new Anchor("https://intranet.uci.cu/");
+        intranet.addClassName("seccion4-link");
+        intranet.add(VaadinIcon.LINK.create());
+        intranet.add("Intranet");
+        intranet.setTarget("_BLANK");
+
+        Anchor periodicoMella = new Anchor("https://periodico.uci.cu/");
+        periodicoMella.addClassName("seccion4-link");
+        periodicoMella.add(VaadinIcon.LINK.create());
+        periodicoMella.add("Periódico Mella");
+        periodicoMella.setTarget("_BLANK");
+
         VerticalLayout otrosSitios = new VerticalLayout(otroSitios, portalUCI, intranet, periodicoMella);
+        /*OTROS SITIOS*/
 
+        /*COMUNIDADES UCI*/
         H2 comunidadessUCI = new H2("COMUNIDADES UCI");
-        HorizontalLayout link7 = new HorizontalLayout(VaadinIcon.LINK.create(), new H3("HumanOS"));
-        link7.setAlignItems(FlexComponent.Alignment.BASELINE);
-        Anchor humanos = new Anchor("https://humanos.uci.cu/", link7);
-        HorizontalLayout link8 = new HorizontalLayout(VaadinIcon.LINK.create(), new H3("FirefoxManía"));
-        link8.setAlignItems(FlexComponent.Alignment.BASELINE);
-        Anchor fireFoxMania = new Anchor("https://firefoxmania.uci.cu/", link8);
-        HorizontalLayout link9 = new HorizontalLayout(VaadinIcon.LINK.create(), new H3("iBlog"));
-        link9.setAlignItems(FlexComponent.Alignment.BASELINE);
-        Anchor blog = new Anchor("https://iblog.uci.cu/", link9);
+        
+        Anchor humanos = new Anchor("https://humanos.uci.cu/");
+        humanos.addClassName("seccion4-link");
+        humanos.add(VaadinIcon.LINK.create());
+        humanos.add("HumanOS");
+        humanos.setTarget("_BLANK");
+        
+        Anchor fireFoxMania = new Anchor("https://firefoxmania.uci.cu/");
+        fireFoxMania.addClassName("seccion4-link");
+        fireFoxMania.add(VaadinIcon.LINK.create());
+        fireFoxMania.add("FirefoxManía");
+        fireFoxMania.setTarget("_BLANK");
+        
+        Anchor blog = new Anchor("https://iblog.uci.cu/");
+        blog.addClassName("seccion4-link");
+        blog.add(VaadinIcon.LINK.create());
+        blog.add("iBlog");
+        blog.setTarget("_BLANK");
+        
         VerticalLayout comunidadesUCI = new VerticalLayout(comunidadessUCI, humanos, fireFoxMania, blog);
+        /*COMUNIDADES UCI*/
 
-        layout.add(sitiosDeInteres, otrosSitios, comunidadesUCI);
-        layout.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
-        layout.addClassName("div-link");
-        layout.setWidthFull();
-
-        return layout;
+        seccion4.add(sitiosDeInteres, otrosSitios, comunidadesUCI);
+        return seccion4;
     }
 
 }
